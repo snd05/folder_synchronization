ccfd92ae540e35a26ccc62ea42bb8b2a26fc68a6
snd05 <teodorescu.alexandru123@gmail.com>
2023-09-25 22:39:51 +0300

    implementing of the hash check method and filecmp

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import sys
+import hashlib
 import shutil
+import filecmp
 
 
 def count_arguments():
@@ -16,6 +18,39 @@ def count_arguments():
     print("\nArguments passed:", end=" ")
     for i in range(1, n):
         print(sys.argv[i], end=" ")
-src=sys.argv[1]
-dst=sys.argv[2]
-shutil.copytree(src,dst)
+
+
+def hash_check(file):
+    # Arbitrary buffer size
+    buf_size = 65536
+    # initializing the sha256() method
+    sha256 = hashlib.sha256()
+
+    with open(file, 'rb') as f:
+        while True:
+            # reading the data = buf_size from the file and saving it in a variable
+            data = f.read(buf_size)
+
+            if not data:
+                break
+            sha256.update(data)
+
+    return sha256.hexdigest()
+
+#choosing the source and destination paths
+src = sys.argv[1]
+dst = sys.argv[2]
+# src_hash = hash_check(src)
+# dst_hash = hash_check(dst)
+# if src_hash == dst_hash:
+#   print("Both files are the same")
+
+# else:
+#   shutil.copy(src,dst)
+#using the filecmp module
+dcmp = filecmp.dircmp(src, dst)
+
+if not dcmp.diff_files and not dcmp.left_only and not dcmp.right_only:
+    print("Both files have the same content")
+else:
+    print("Folders have different content")
