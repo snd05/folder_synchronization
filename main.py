1b268bd4f35c0e9a6f1f21c9d0ff810f79189682
snd05 <teodorescu.alexandru123@gmail.com>
2023-09-26 23:09:52 +0300

    finalizing the project

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import filecmp
 import time
 from datetime import datetime
 
+
 def count_arguments():
     # number of arguments
     n = len(sys.argv)
@@ -11,7 +12,6 @@ def count_arguments():
         print(
             "The number of arguments is not valid\nThe syntax is: python script.py source_folder destination_folder log_file_path sync_interval")
         sys.exit()
-    print("Total arguments passed:", n)
     # name of the script
     print("\nName of the python script:", sys.argv[0])
     # arguments passed
@@ -20,13 +20,15 @@ def count_arguments():
 # using the filecmp module
 def compare_folders(src, dst):
     dcmp = filecmp.dircmp(src, dst)
-
     if not dcmp.diff_files and not dcmp.left_only and not dcmp.right_only:
-        log_file.write("Both files have the same content\n")
+        log_file.write("Both folders have the same content\n")
+        print("Both folders have the same content\n")
     else:
         log_file.write("Replacing the content of the destination folder...\n")
         shutil.rmtree(dst)
         shutil.copytree(src, dst)
+        print("Changes made at ", datetime.now())
+
 
 count_arguments()
 # choosing the source and destination paths
@@ -43,7 +45,6 @@ while True:
     log_file.write(f"{datetime.now()} - Checking the folder at the interval given...\n")
     # comparing the folders
     compare_folders(src, dst)
-
     log_file.flush()
     # waiting to check again
     time.sleep(sync_interval)
